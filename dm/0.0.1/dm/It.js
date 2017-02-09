0f30d0e5e0da846b65d9d14649099aec2bd916da
deme <deme@debian.deme.es>
2017-02-09 22:55:05 +0100

    It finished

--- dm/0.0.1/dm/It.js
+++ dm/0.0.1/dm/It.js
@@ -6,18 +6,17 @@
 
 (() => {
   "use strict";
+  let Tp = dm.Tp;
 
   //# T :: ( - bool) - ( - T) - It
   dm.It = function (hasNext, next) {
-    var self;
-    self = this;
+    let self = this;
 
     /// Returns [this] with [element] added at end or at [i] if [i] is
     /// not null
     //# T - ?num - !It<T>
     this.add = (e, i) => {
-      var c;
-      c = 0;
+      let c = 0;
       if (!i) return new dm.It(
         () => c == 0,
         () => {
@@ -43,8 +42,7 @@
     /// Returns [this] with [e] added at begin
     //# T - !It<T>
     this.add0 = e => {
-      var isNotAdd;
-      isNotAdd = true
+      let isNotAdd = true
       return new dm.It(
         () => hasNext() || isNotAdd,
         () => {
@@ -59,8 +57,7 @@
     /// Inserts an iterator in [this] at [i] or at end if [i] is null.
     //# !It<T> - num= - !It<T>
     this.addIt = (it, i) => {
-      var c;
-      c = 0;
+      let c = 0;
       return new dm.It(
         () => hasNext() || it.hasNext(),
         () => {
@@ -80,11 +77,36 @@
         });
     }
 
+    /// Returns <b>true</b> if all elements give <b>true</b> with [f]
+    //# (T - bool) - bool
+    this.all = f => {
+      while (hasNext()) if (!f(next())) return false;
+      return true;
+    }
+
+    /// Returns <b>true</b> if any element of [this] is equals (===) to [e]
+    //# T - bool
+    this.contains = e => {
+      while (hasNext()) if (e === next()) return true;
+      return false;
+    }
+
+    /// Returns <b>true</b> if any element of [this] returns true with [f]
+    //# func(T):bool - bool
+    this.containsf = f => {
+      while (hasNext()) if (f(next())) return true;
+      return false;
+    }
+
+    /// Returns the number of elements whish give <b>true</b> with [f] <p>
+    /// Number of all the elements is returned by [size()]
+    //# (T - bool) - num
+    this.count = f => self.filter(f).size();
+
     /// Returns rest of [this] after call [take ()]
     //# num - !It<T>
     this.drop = n => {
-      var r;
-      r = self.take(n);
+      let r = self.take(n);
       while (r.hasNext())
         r.next();
       return self;
@@ -97,9 +119,8 @@
     /// Returns rest of It after call[ takeUntil()]
     //# (T - bool) - !It<T>
     this.dropUntil = f => {
-      var last, nx;
-      last = null;
-      nx = true;
+      let last = null;
+      let nx = true;
       while (true)
         if (hasNext()) {
           last = next();
@@ -111,8 +132,7 @@
       return new dm.It(
         () => nx,
         () => {
-          var r;
-          r = last;
+          let r = last;
           if (hasNext()) last = next();
           else nx = false;
           return r;
@@ -124,8 +144,7 @@
 
     //# (T - num - ) -
     this.eachIx = f => {
-      var c;
-      c = -1;
+      let c = -1;
       while (hasNext())
         f(next(), ++c);
     }
@@ -150,11 +169,159 @@
       return true;
     }
 
+    /**
+     * Filters [this], returning a subset of collection.
+     *   f      : Function to select values
+     */
+    //# (T - bool) - !It<T>
+    this.filter = (f) => {
+      let last = null
+      let nx = true
+      let nnext = () => {
+        while (true)
+          if (hasNext()) {
+            last = next();
+            if (f(last)) break;
+          } else {
+            nx = false;
+            break;
+          }
+      }
+      nnext();
+      return new It(
+        () => nx,
+        () => {
+          let r = last;
+          nnext();
+          return r
+        });
+    }
+
+    /// Returns those elements that gives <b>true</b> with [f].
+    //# (T - bool) - !Arr<T>
+    this.find = f => {
+      let a = []
+      self.each(e => { if (f(e)) a.push(e); });
+      return a;
+    }
+
+    /// Returns the first element that gives <b>true</b> with [f] or null
+    //# (T - bool) - T
+    this.findFirst = f => {
+      while (hasNext()) {
+        let e = next();
+        if (f(e)) return e;
+      }
+      return null;
+    }
+
     //# - bool
     this.hasNext = () => hasNext();
 
+    /// Returns the index of first element that is equals (===) to [e]
+    //# T - num
+    this.index = e => {
+      let i = 0;
+      while (hasNext()) {
+        if (next() === e) return i;
+        ++i;
+      }
+      return -1;
+    }
+
+    /// Returns the index of first element that gives <b>true</b> with [f]
+    /// or -1 if [this] has nothing
+    //# (T - bool) - num
+    this.indexf = f => {
+      let i = 0;
+      while (hasNext()) {
+        if (f(next())) return i;
+        ++i;
+      }
+      return -1;
+    }
+
+    /// Returns the index of first element that is equals (===) to [e]
+    //# T - num
+    this.lastIndex = o => {
+      let r = -1;
+      let i = 0;
+      self.each(e => {
+        if (e == o) r = i;
+        ++i;
+      });
+      return r;
+    }
+
+    /// Returns the index of last element that gives <b>true</b> with [f]
+    /// or -1 if [this] has nothing
+    //# (T - bool) - num
+    this.lastIndexf = f => {
+      let r = -1;
+      let i = 0;
+      self.each(e => {
+        if (f(e)) r = i;
+        ++i;
+      });
+      return r;
+    }
+
+    /// Returns the iterator whish results of apply 'f' to every element
+    /// of 'this'
+    //# U :: (T - U) - !It<U>
+    this.map = f => new It (() => hasNext(), () => f(next()));
+
     //# - T
     this.next = () => next();
+
+    /// Returns the result of applying [f]([f]([seed], e1), e2)... over
+    /// every element of [this].
+    //# R :: R - (R - T - R) - R
+    this.reduce = (seed, f) => {
+      while (hasNext()) seed = f(seed, next());
+      return seed;
+    }
+
+    /// Returns [this] in reverse order
+    ///   Creates an array!
+    //# - !It<T>
+    this.reverse = () => It.from(self.to().reverse());
+
+    /// Returns the number of elements of [this].
+    //# - num
+    this.size = () => {
+      let r = 0
+      self.each(e => ++r);
+      return r;
+    }
+
+    /// Sort [this] in natural order (lowercase after uppercase)
+    ///   Creates an array!
+    //# - !It<T>
+    this.sort = () => It.from(self.to().sort());
+
+    /// Sort [this] conforming [compare] function
+    ///   Creates an array!
+    //# (T - T - num) - !It<T>
+    this.sortf = f => It.from(self.to().sort(f));
+
+    /// Returns an iterator over elements of [this] mixed
+    ///   Creates an array!
+    //# - !It<T>
+    this.shuffle = () => {
+      let a = self.to();
+      let i = a.length;
+      if (!i) return It.empty();
+      while (--i) {
+        let ni = Math.floor(Math.random() * (i + 1));
+        if (ni !== i) {
+          let tmp = a[i];
+          a[i] = a[ni];
+          a[ni] = tmp;
+        }
+      }
+      return It.from(a);
+    }
 
     /**
      * Returns n first elements.<p>
@@ -172,9 +339,8 @@
     /// Returns the first elements of [it] whish give <b>true</b> with [f]
     //# (T - bool) - !It<T>
     this.takeWhile = f => {
-      var last, hnx;
-      last = null;
-      hnx = false;
+      let last = null;
+      let hnx = false;
       if (hasNext()) {
         last = next();
         hnx = true;
@@ -182,8 +348,7 @@
       return new dm.It(
         () => hnx && f(last),
         () => {
-          var r;
-          r = last;
+          let r = last;
           last = null;
           hnx = false;
           if (hasNext()) {
@@ -200,8 +365,7 @@
 
     //# - !Arr<T>
     this.to = () => {
-      var a;
-      a = [];
+      let a = [];
       self.each(o => a.push(o));
       return a;
     }
@@ -217,50 +381,114 @@
 
   //# - !It<?>
   It.empty = () => new It (() => false, () => null);
+
   /// Create an It from an array
   //# T :: Arr<T> - It<T>
   It.from = a => {
-    var i, len;
-    i = 0;
-    len = a.length;
-    return new It(
-      () => i < len,
-      () => a[i++]
-    );
+    let i = 0;
+    let len = a.length;
+    return new It(() => i < len, () => a[i++]);
   }
+
   /// Return an [It] over a Bytes object.
   //# Uint8Array - !It<num>
   It.fromBytes = bs => {
-    var l, c;
-    l = bs.length;
-    c = 0;
+    let l = bs.length;
+    let c = 0;
     return new It (() => c < l, () => bs[c++]);
   }
+
   /// Returns an [It] over String characters.
   //# str - !It<str>
   It.fromStr = s => {
-    var l, c;
-    l = s.length;
-    c = 0;
+    let l = s.length;
+    let c = 0;
     return new It (() => c < l, () => s[c++]);
   }
+
   //# !It<str> - ?str - str
   It.join = (i, sep) => {
-    var r;
     if (!sep) sep = "";
     if (!i.hasNext()) return "";
-    r = i.next();
+    let r = i.next();
     i.each(o => r += sep + o);
     return r;
   }
+
   //# K, V :: !Obj<K, V> - !It<K>
   It.keys = o => {
-    var tmp, k;
-    tmp = [];
-    for (k in o)
+    let tmp = [];
+    for (let k in o)
       tmp.push(k);
     return It.from(tmp)
   }
+
+  /**
+   * Iterates over a range. If end is null the range is [0 - startEnd),
+   * otherwise the range is [starEnd - end)
+   */
+  //# num - ?num - !It<num>
+  It.range = (startEnd, end) => {
+    if (!end) {
+      end = startEnd;
+      startEnd = 0;
+    }
+
+    if (startEnd < 0)
+      startEnd = 0;
+
+    return new It(() => startEnd < end, () => startEnd++);
+  }
+
+  /// Sorts an It of strings in locale
+  //# !It<str> - !It<str>
+  It.sortl = i => i.sortf((e1, e2) => e1.localeCompare(e2));
+
+  /// Returns two iterators from one It<Tp2>
+  ///   Creates two arrays!
+  //# A, B :: !It<!Tp<A, B>> - !Tp<!It<A>,!It<B>>
+  It.unzip = i => {
+    let a1 = [];
+    let a2 = [];
+    i.each(tp => {
+      a1.push(tp._1);
+      a2.push(tp._2);
+    });
+    return new Tp(It.from(a1), It.from(a2));
+  }
+
+  /// Returns three iterators from one It<Tp3>
+  ///   Creates three arrays!
+  //# A, B, C :: !It<!Tp3<A, B, C>> - !Tp3<!It<A>,!It<B>,!It<C>>
+  It.unzip3 = i => {
+    let a1 = [];
+    let a2 = [];
+    let a3 = [];
+    i.each(tp => {
+      a1.push(tp._1);
+      a2.push(tp._2);
+      a3.push(tp._3);
+    });
+    return new Tp(It.from(a1), It.from(a2), It.from(a3));
+  }
+
+  /// Returns a iterator with elements of [it1] and [it2].<p>
+  /// The number of elements of resultant iterator is the least of both ones.
+  //# A, B :: !It<A> - !It<B> - !It<!Tp<A, B>>
+  It.zip = (i1, i2) =>
+    new It(
+      () => i1.hasNext() && i2.hasNext(),
+      () => new Tp(i1.next(), i2.next())
+    );
+
+  /// Returns a iterator with elements of [it1], [it2] and [it3].<p>
+  /// The number of elements of resultant iterator is the least of three ones.
+  //# A, B, C :: !It<A> - !It<B> - !It<C> - !It<!Tp3<A, B, C>>
+  It.zip3 = (i1, i2, i3) =>
+    new It(
+      () => i1.hasNext() && i2.hasNext() && i3.hasNext(),
+      () => new Tp(i1.next(), i2.next(), i3.next())
+    );
 
 })();
 
--- test/0.0.1/src/it.js
+++ test/0.0.1/src/it.js
@@ -107,6 +107,146 @@ it = () => {
   t.eq("[]", It.from(i1).dropWhile(pr1).toString());
   t.eq("[2, 3]", It.from(i2).dropWhile(pr1).toString());
 
+  let even = e => e % 2 == 0;
+  let neven = e => e % 2 != 0
+
+  t.eq("[]", It.from(i0).filter(even).toString());
+  t.eq("[]", It.from(i1).filter(even).toString());
+  t.eq("[2]", It.from(i2).filter(even).toString());
+  t.eq("[]", It.from(i0).filter(neven).toString());
+  t.eq("[1]", It.from(i1).filter(neven).toString());
+  t.eq("[1, 3]", It.from(i2).filter(neven).toString());
+
+  let mul2 = e => e * 2;
+
+  t.eq("[]", It.from(i0).map(mul2).toString());
+  t.eq("[2]", It.from(i1).map(mul2).toString());
+  t.eq("[2, 4, 6]", It.from(i2).map(mul2).toString());
+
+  // Progresive ----------------------
+
+  t.mark("progresive");
+
+  let ftrue = e => true;
+  let feq = a => e => a == e;
+
+  t.yes(It.from(i0).all(feq(1)));
+  t.yes(It.from(i1).all(feq(1)));
+  t.yes(!It.from(i2).all(feq(1)));
+  t.yes(!It.from(i0).containsf(feq(1)));
+  t.yes(It.from(i2).containsf(feq(1)));
+  t.yes(!It.from(i2).containsf(feq(9)));
+  t.yes(!It.from(i0).contains(1));
+  t.yes(It.from(i2).contains(1));
+  t.yes(!It.from(i2).contains(9));
+
+  t.eq(0, It.from(s0).count(ftrue));
+  t.eq(1, It.from(s1).count(ftrue));
+  t.eq(3, It.from(s2).count(ftrue));
+
+  t.eq(0, It.from(i0).find(even).length);
+  t.eq(0, It.from(i1).find(even).length);
+  t.eq(2, It.from(i2).find(even)[0]);
+  t.eq(0, It.from(i0).find(neven).length);
+  t.eq(1, It.from(i1).find(neven)[0]);
+  t.eq(1, It.from(i2).find(neven)[0]);
+
+  t.eq(-1, It.from(i0).indexf(even));
+  t.eq(-1, It.from(i1).indexf(even));
+  t.eq(1, It.from(i2).indexf(even));
+  t.eq(-1, It.from(i0).indexf(neven));
+  t.eq(0, It.from(i1).indexf(neven));
+  t.eq(0, It.from(i2).indexf(neven));
+
+  t.eq(-1, It.from(i0).index(3));
+  t.eq(-1, It.from(i1).index(3));
+  t.eq(0, It.from(i1).index(1));
+  t.eq(-1, It.from(i2).index(5));
+  t.eq(0, It.from(i2).index(1));
+  t.eq(2, It.from(i2).index(3));
+
+  t.eq(-1, It.from(i0).lastIndexf(even));
+  t.eq(-1, It.from(i1).lastIndexf(even));
+  t.eq(1, It.from(i2).lastIndexf(even));
+  t.eq(-1, It.from(i0).lastIndexf(neven));
+  t.eq(0, It.from(i1).lastIndexf(neven));
+  t.eq(2, It.from(i2).lastIndexf(neven));
+
+  t.eq(-1, It.from(i0).lastIndex(3));
+  t.eq(-1, It.from(i1).lastIndex(3));
+  t.eq(0, It.from(i1).lastIndex(1));
+  t.eq(-1, It.from(i2).lastIndex(5));
+  t.eq(0, It.from(i2).lastIndex(1));
+  t.eq(2, It.from(i2).lastIndex(3));
+
+  // Block ------------------
+
+  t.mark("block");
+
+  t.eq("[]", It.from(i0).reverse().toString());
+  t.eq("[1]", It.from(i1).reverse().toString());
+  t.eq("[3, 2, 1]", It.from(i2).reverse().toString());
+
+  let arr = ["pérez", "pera", "p zarra", "pizarra"];
+  t.eq(
+    ["p zarra", "pera", "pizarra", "pérez"].toString(),
+    It.from(arr).sort().to().toString()
+  );
+  t.eq(
+    ["p zarra", "pera", "pérez", "pizarra"].toString(),
+    It.sortl(It.from(arr)).to().toString()
+  );
+
+  t.eq("[]", It.from(s0).shuffle().toString());
+  t.eq("[one]", It.from(s1).shuffle().toString());
+  // console.log(It.from(s2).shuffle().toString());
+
+  // Static constructors ------------------
+
+  t.mark("static constructors");
+
+  t.eq("[0, 1, 2, 3, 4]", It.range(5).toString());
+  t.eq("[2, 3, 4]", It.range(2, 5).toString());
+  t.eq("[]", It.range(0).toString());
+  t.eq("[]", It.range(2, 2).toString());
+
+  t.eq("[]", It.zip(It.from(s0), It.from(s2)).toString());
+  let its = It.unzip(It.zip(It.from(s1), It.from(s2)));
+  t.yes(its._1.eq(It.from(s1)));
+  t.yes(its._2.eq(It.from(s1)));
+  its = It.unzip(It.zip(It.from(i1), It.from(i2)));
+  t.yes(its._1.eq(It.from(i1)));
+  t.yes(its._2.eq(It.from(i1)));
+
+  let sum = 0;
+  It.from(i0).each(e => sum += e);
+  t.eq(0, sum);
+  It.from(i1).each(e => sum += e);
+  t.eq(1, sum);
+  It.from(i2).each(e => sum += e);
+  t.eq(7, sum);
+
+  let sum2 = 0;
+  sum = 0;
+  It.from(i0).eachIx((e, ix) => {
+    sum += e;
+    sum2 += ix;
+  });
+  t.eq(0, sum);
+  t.eq(0, sum2);
+  It.from(i1).eachIx((e, ix) => {
+    sum += e;
+    sum2 += ix;
+  });
+  t.eq(1, sum);
+  t.eq(0, sum2);
+  It.from(i2).eachIx((e, ix) => {
+    sum += e;
+    sum2 += ix;
+  });
+  t.eq(7, sum);
+  t.eq(3, sum2);
+
   t.log();
 }
 
